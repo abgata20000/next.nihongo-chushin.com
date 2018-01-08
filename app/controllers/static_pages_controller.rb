03745b01ca102e2f1cc380a140df95f7bdc3a060
abgata20000 <abgata@abgata.jp>
2018-01-08 23:52:32 +0900

    Add: 公開部屋のコメントをTOPに表示する
    Issue:

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,19 @@
 class StaticPagesController < ApplicationController
+  before_action :set_top_variables, only: %w(top)
+  SHOW_COMMENT_NUM = 100
   def top
-    redirect_to rooms_path
+
+  end
+
+
+  private
+
+  def set_top_variables
+    @room = Room.enabled.where(is_fixed: true, is_public: true).first
+    if @room
+      @chats = @room.chats.enabled.where(system_message: false).order(updated_at: :desc).limit(SHOW_COMMENT_NUM)
+    else
+      redirect_to rooms_path
+    end
   end
 end
